108a5db5ceaebefbfa332cc1bd42396c1d03215f
Sally Le (TECHVIFY.D3) <sally.le@techvify.com.vn>
2026-04-15 10:27:33 +0700

    update

--- pages/ODashboard.tsx
+++ pages/ODashboard.tsx
@@ -928,7 +928,7 @@ const ODashboard: React.FC = () => {
                       <ResponsiveContainer width="100%" height={280}>
                         <BarChart
                           data={logworkTrendData}
-                          barGap={isSingleMonthSelected ? -300 : -20}
+                          barGap={isSingleMonthSelected ? -300 : -10}
                           barCategoryGap={isSingleMonthSelected ? "20%" : "20%"}
                           margin={{ top: 20, right: 20, bottom: 5, left: 0 }}
                         >
